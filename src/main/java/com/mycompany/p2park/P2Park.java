809ae46104a40e361c2452cc5d0530e10d153f86
RPCS3 <RPCS3@RPCS3-PC>
2024-05-19 16:55:14 +0800

    TEST FIN

---
+++ src/main/java/com/mycompany/p2park/P2Park.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.p2park;
+
+/**
+ *
+ * @author Matthew
+ */
+public class P2Park {
+
+    public static void main(String[] args) {
+        
+        LoginForm loginform = new LoginForm();
+        loginform.setVisible(true);
+        
+            }
+}
